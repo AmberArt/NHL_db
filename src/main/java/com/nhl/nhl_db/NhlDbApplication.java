8da4a79e942c0bf80f636768b91ab9f9ef2b7cde
A_biggart <103457162+AmberArt@users.noreply.github.com>
2022-07-08 16:27:09 -0600

    Add files via upload

---
+++ src/main/java/com/nhl/nhl_db/NhlDbApplication.java
@@ -0,0 +1,13 @@
+package com.nhl.nhl_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhlDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhlDbApplication.class, args);
+	}
+
+}
